3cb50722a51ff723db3367748b5a68b16c514e38
CheretKa <ocheretianfws@gmail.com>
2021-04-14 20:27:18 +0300

    test 1

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -2,6 +2,7 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static java.util.Objects.nonNull;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,18 +12,23 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductDataBuilder;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import java.lang.reflect.InvocationHandler;
 
 @ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
 
-    private static final String SAMPLE_CLIENT_NAME = "Kowalski";
+
     @Mock
     private InvoiceFactory factory;
     @Mock
     private TaxPolicy taxPolicy;
+
     private BookKeeper keeper;
 
     @BeforeEach
@@ -30,23 +36,20 @@ class BookKeeperTest {
         keeper = new BookKeeper(factory);
     }
 
-    ProductData
 
     @Test
-    void test() {
-        Id sampleId = Id.generate();
-        ClientData dummy = new ClientData(sampleId, SAMPLE_CLIENT_NAME);
-        InvoiceRequest request = new InvoiceRequest(dummy);
+    void when_RequestOfInvoiceWithOneElement_Expect_ReturnInvoiceWithOneItem() {
+//        Test case 1
+        ClientData sampleClientData = new ClientData(Id.generate(), "Kowalski");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(sampleClientData);
+        invoiceRequest.add(new RequestItemBuilder().setQuantity(1).build());
 
-        Invoice invoice = new Invoice(Id.generate(), dummy);
-        when(factory.create(dummy)).thenReturn(invoice);
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(Money.ZERO, "tax"));
+        when(factory.create(sampleClientData)).thenReturn(new Invoice(Id.generate(), sampleClientData));
 
-
-        Invoice issuance =  keeper.issuance(request, taxPolicy);
-
-        assertTrue(nonNull(issuance));
-        assertEquals(invoice, issuance);
-
+        Invoice returnedInvoice = keeper.issuance(invoiceRequest, taxPolicy);
+        assertTrue(nonNull(returnedInvoice));
+        assertEquals(1, returnedInvoice.getItems().size());
     }
 
 }
